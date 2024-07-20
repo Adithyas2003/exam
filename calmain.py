9a9352f29c31b46060967b9ea3cd140de41d8da8
Adithyas2003 <adathyas49@gmail.com>
2024-07-20 11:38:59 +0530

    new file added

---
+++ calmain.py
@@ -0,0 +1,30 @@
+import calculator
+def main():
+        while True:
+             print("calculator")
+             print("1.add")
+             print("2.sub")
+             print("3.mul")
+             print("4.div")
+             print("5.exit")
+             ch=input("enter your choice")
+             if ch==5:
+                    print("exit")
+                    break
+        a=int(input("enter the frst no"))
+        b=int(input("enter second no"))
+        if ch==1:
+               print(num(a,b))
+        if ch==2:
+               print(num(a,b))
+        if ch==3:
+               print(num(a,b))
+        if ch==4:
+               print(num(a,b))
+        if ch==5:
+               print(num(a,b))
+
+              
+        
+             
+    
---
+++ pattern.py
@@ -0,0 +1,7 @@
+a=65
+for i in range (a):
+    if i==a:
+        for j in  range(i):
+            print(str(a),end='/t')
+            a+=1
+print(i)
